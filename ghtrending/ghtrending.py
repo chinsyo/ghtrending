b35264c0acf0b724d0b8d71680b00b3bf6856cdd
Chinsyo <chinsyo@sina.cn>
2017-12-14 17:55:19 +0800

    oop

--- ghtrending/ghtrending.py
+++ ghtrending/ghtrending.py
@@ -22,75 +22,131 @@ headers = {
     'Accept-Encoding': 'gzip',
 }
 
-
 def _print_sectiontitle(title):
-    print('=' * 60)
+    _print_separateline('=')
     print(time.strftime('%Y-%m-%d', time.localtime()), title)
-    print('=' * 60)
+    _print_separateline('=')
 
 
-def _print_separateline():
-    print('-' * 60)
+def _print_separateline(separator='-'):
+    print(separator * 60)
 
 
 def _xpath_textornull(el, stmt):
     tags = el.xpath(stmt)
     return tags[-1] if len(tags) else '<null>'
 
+class GHRepo(object):
+    def __init__(self, html):
+        self.today = _xpath_textornull(html, './/svg[@class="octicon octicon-star"]/parent::node()/text()')
+        self.name = _xpath_textornull(html, './/div[contains(@class, "col-9")]/h3/a/@href')
+        self.desc = _xpath_textornull(html, './/div[@class="py-1"]/p/text()')
+        self.star = _xpath_textornull(html, './/svg[contains(@aria-label, "star")]/parent::node()/text()')
+        self.fork = _xpath_textornull(html, './/svg[contains(@aria-label, "fork")]/parent::node()/text()')
 
-def _gettrending_repository(html):
-    repo_list = html.xpath('//ol[@class="repo-list"]/li')
-    _print_sectiontitle('Top {} Github Trending Repository'.format(len(repo_list)))
+    def __str__(self):
+        description = ""
+        description += "* 🌍 No.{} {} ({})".format(self.index + 1, self.name.strip()[1:], self.today.strip())
+        description += "\n"
+        description += "* 🌟 star: {} \t🍴 fork: {}".format(self.star.strip(), self.fork.strip())
+        description += "\n"
+        description += "* 📚 desc: {}".format(self.desc.strip())
+        return description
 
-    for index, repo in enumerate(repo_list):
-        today = _xpath_textornull(repo, './/svg[@class="octicon octicon-star"]/parent::node()/text()')
-        name = _xpath_textornull(repo, './/div[contains(@class, "col-9")]/h3/a/@href')
-        desc = _xpath_textornull(repo, './/div[@class="py-1"]/p/text()')
-        star = _xpath_textornull(repo, './/svg[contains(@aria-label, "star")]/parent::node()/text()')
-        fork = _xpath_textornull(repo, './/svg[contains(@aria-label, "fork")]/parent::node()/text()')
+    @property
+    def index(self):
+        return self._index
+
+    @index.setter
+    def index(self, v):
+        self._index = v if v >= 0 else 0
+
+
+class GHUser(object):
+    def __init__(self, html):
+        self.name = html.xpath('.//div[@class="mx-2"]/h2/a/text()')[0]
+        self.repo = _xpath_textornull(html, './/span[contains(@class, "repo-snipit-name")]/span/text()')
+        self.desc = _xpath_textornull(html, './/span[contains(@class, "repo-snipit-description")]/text()')
+
+    def __str__(self):
+        description = ""
+        description += "* 🌍 No.{} {}".format(self.index + 1, self.name.strip())
+        description += "\n"
+        description += "* 📦 repo: {}".format(self.repo.strip())
+        description += "\n"
+        description += "* 📚 desc: {}".format(self.desc.strip())
+        return description
+
+    @property
+    def index(self):
+        return self._index
+
+    @index.setter
+    def index(self, v):
+        self._index = v if v >= 0 else 0
+
+
+class GHClient(object):
+    def __init__(self, qtype=0, since='today', lang=None):
+        self.qtype = qtype
+        self.since = since
+        self.lang = lang
+
+    def request(self):
+        html = self._getcontent()
+        if self.qtype == 0:
+            self._parse_trending_repository(html)
+        else:
+            self._parse_trending_developers(html)
+
+    def _getcontent(self):
+        assert self.qtype < len(GHTRENDING_QTYPE)
+        assert self.since in GHTRENDING_SINCE
+
+        params = {'since': self.since} if (self.since is not 'today') else None
+
+        url = GHTRENDING_ROOT_URL
+        url = urljoin(url, GHTRENDING_QTYPE[self.qtype])
+        url = urljoin(url, self.lang)
+
+        request = requests.get(url, headers=headers, params=params)
+        request.encoding = 'utf-8'
+        assert request.status_code == 200
+
+        html = etree.HTML(request.text)
+        return html
+
+    def _parse_trending_repository(self, html):
+        repo_list = html.xpath('//ol[@class="repo-list"]/li')
+        _print_sectiontitle('Top {} Github Trending Repository'.format(len(repo_list)))
+
+        for index, repo in enumerate(repo_list):
+            r = GHRepo(repo)
+            r.index = index
+
+            _print_separateline()
+            print(r)
 
         _print_separateline()
-        print("* No.{} {} ({})".format(index + 1, name.strip()[1:], today.strip()))
-        print("* star: {} \tfork: {}".format(star.strip(), fork.strip()))
-        print("* desc: {}".format(desc.strip()))
-    _print_separateline()
 
 
-def _gettrending_developers(html):
-    developers = html.xpath('//ol[@class="list-style-none"]/li')
-    _print_sectiontitle('Top {} Github Trending Developers'.format(len(developers)))
+    def _parse_trending_developers(self, html):
+        developers = html.xpath('//ol[@class="list-style-none"]/li')
+        _print_sectiontitle('Top {} Github Trending Developers'.format(len(developers)))
 
-    for index, developer in enumerate(developers):
+        for index, developer in enumerate(developers):
+            u = GHUser(developer)
+            u.index = index
+
+            _print_separateline()
+            print(u)
+
         _print_separateline()
-        name = developer.xpath('.//div[@class="mx-2"]/h2/a/text()')[0]
-        repo = _xpath_textornull(developer, './/span[contains(@class, "repo-snipit-name")]/span/text()')
-        desc = _xpath_textornull(developer, './/span[contains(@class, "repo-snipit-description")]/text()')
-        print('* No.{} {}'.format(index + 1, name.strip()))
-        print('* repo: {}'.format(repo.strip()))
-        print('* desc: {}'.format(desc.strip()))
-    _print_separateline()
 
 
 def main():
     args = ARGS.parse_args()
-    assert args.qtype < len(GHTRENDING_QTYPE)
-    assert args.since in GHTRENDING_SINCE
-
-    params = {'since': args.since} if (args.since is not 'today') else None
-
-    url = GHTRENDING_ROOT_URL
-    url = urljoin(url, GHTRENDING_QTYPE[args.qtype])
-    url = urljoin(url, args.lang)
-
-    request = requests.get(url, headers=headers, params=params)
-    request.encoding = 'utf-8'
-    assert request.status_code == 200
-
-    html = etree.HTML(request.text)
-    if args.qtype == 0:
-        _gettrending_repository(html)
-    else:
-        _gettrending_developers(html)
+    GHClient(args.qtype, args.since, args.lang).request()
 
 
 ARGS = argparse.ArgumentParser(description='Github Trending')
@@ -101,3 +157,4 @@ ARGS.add_argument('-s', '--since', dest='since', default='today', action='store'
 ARGS.add_argument('-l', '--lang', dest='lang', action='store', type=str, help='Specity language')
 if __name__ == '__main__':
     main()
+    
