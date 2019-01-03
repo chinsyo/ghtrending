bb6f7226182659d9cf9723d486f3c94f158e5823
Chinsyo <chinsyo@sina.cn>
2019-01-03 16:21:54 +0800

    bump version

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 requires = [
     'lxml',
@@ -7,12 +10,15 @@ requires = [
 
 setup(
     name='ghtrending',
-    version='0.1.4',
-    license='MIT',
-    description='Github Trending',
+    version='0.1.5rc',
     author='chinsyo',
     author_email='chinsyo@sina.cn',
-    packages=['ghtrending'],
+    license='MIT',
+    description='Github Trending Explorer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chinsyo/ghtrending",
+    packages=find_packages(),
     package_dir={'ghtrending': 'ghtrending'},
     package_data={'': ['LICENSE', 'NOTICE']},
     include_package_data=True,
